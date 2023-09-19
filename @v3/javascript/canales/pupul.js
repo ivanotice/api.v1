4d50ab9da4347caa65035de28823cdbf99c40de3
Enyerson <135800644+ivanotice@users.noreply.github.com>
2023-09-19 01:35:19 -0400

    Update pupul.js

--- @v3/javascript/canales/pupul.js
+++ @v3/javascript/canales/pupul.js
@@ -4,21 +4,27 @@ $(document).ready(function() {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1000,
-    arrows: false, // Desactivar los botones de siguiente y anterior
+    arrows: false,
     draggable: true,
     infinite: true,
     swipeToSlide: true,
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 992, // Cambié el breakpoint a 992 para dispositivos de tamaño mediano
         settings: {
           slidesToShow: 3
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
         }
       }
     ]
