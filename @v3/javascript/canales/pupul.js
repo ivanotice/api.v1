e83e6f0d811cfb7b6b6100d60154d30a24fd6f15
Enyerson <135800644+ivanotice@users.noreply.github.com>
2023-09-19 01:40:55 -0400

    Update pupul.js

--- @v3/javascript/canales/pupul.js
+++ @v3/javascript/canales/pupul.js
@@ -1,34 +1,41 @@
 $(document).ready(function() {
-  $('.carousel .slides').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    arrows: false,
-    draggable: true,
-    infinite: true,
-    swipeToSlide: true,
-    responsive: [
-      {
-        breakpoint: 992, // Cambié el breakpoint a 992 para dispositivos de tamaño mediano
-        settings: {
-          slidesToShow: 3
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    $('.carousel .slides').slick({
+      slidesToShow: 7, // Aumenta la cantidad de elementos a 7 en dispositivos móviles
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      draggable: true,
+      infinite: true,
+      swipeToSlide: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 5 // Reduce la cantidad de elementos a 5 en tabletas
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3 // Reduce la cantidad de elementos a 3 en dispositivos móviles pequeños
+          }
         }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+      ]
+    });
+  } else {
+    $('.carousel .slides').slick({
+      slidesToShow: 5, // Mantiene la cantidad de elementos en 5 en Windows y otros dispositivos
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      draggable: true,
+      infinite: true,
+      swipeToSlide: true
+    });
+  }
 
   $('.carousel .slides').on('swipe', function(event, slick, direction) {
     if (direction === 'left') {
